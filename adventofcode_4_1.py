3e2b7187593abe1a2b1e9c9205e26e15fdf37ebe
sanr4kh <fieldstreet.27@gmail.com>
2023-12-05 01:02:56 +0300

    day 4 done

--- adventofcode_4_1.py
+++ adventofcode_4_1.py
@@ -1,15 +1,3 @@
-def find_l(i, j, data):
-    pass
-
-
-def find_r(i, j, data):
-    pass
-
-
-def find_o(i, j, data):
-    pass
-
-
 def adventofcode_4_1():
     '''
     o
@@ -18,46 +6,70 @@ def adventofcode_4_1():
     with open('inputtext.txt') as file:
         data = file.read()
     data = data.strip().split('\n')
-    mult = 0
 
-    for i, line in enumerate(data):
-        if '*' in line:
-            if i == 0:
-                pass
-            if i == len(line) - 1:
-                pass
-            new_gear = []
-            for j, char in enumerate(line):
-                if data[i][j] == '*':
-                    new_gear = []
-                    if data[i][j - 1] in '0123456789':
-                        try:
-                            new_gear += find_l(i, j, data)
-                        except TypeError:
-                            pass
-                    if data[i][j + 1] in '0123456789':
-                        try:
-                            new_gear += find_r(i, j, data)
-                        except TypeError:
-                            pass
-                    if i > 0:
-                        try:
-                            new_gear += find_o(i - 1, j, data)
-                        except TypeError:
-                            pass
+    points_total = 0
 
-                    if i < len(data) - 1:
-                        try:
-                            new_gear += find_o(i + 1, j, data)
-                        except TypeError:
-                            pass
-                if len(new_gear) == 2:
-                    mult += new_gear[0] * new_gear[1]
-                new_gear = []
+    for card in data:
+        edited_card = card.replace('  ', ' ')
+        card_win_numbers, card_numbers = edited_card.split('|')
+        card_win_numbers = card_win_numbers.strip().split(':')[1].strip().split(' ')
+        card_numbers = card_numbers.strip().split(' ')
+        card_numbers = [int(i) for i in card_numbers]
+        card_win_numbers = [int(i) for i in card_win_numbers]
 
-    return mult
+        hits = len([i for i in card_win_numbers if i in card_numbers])
+
+        if hits > 0:
+            points_total += 2**(hits - 1)
+
+    return points_total
+
+
+def adventofcode_4_2():
+    '''
+    o
+    '''
+
+    with open('inputtext.txt') as file:
+        data = file.read()
+    data = data.strip().split('\n')
+
+    card_nums = len(data)
+    card_deck = dict(zip(range(card_nums), (1 for _ in range(card_nums))))
+
+    cards_total = 0
+    points_total = 0
+
+    for num, card in enumerate(data):
+        step = 0
+
+        print('for num, card in enumerate(data): ', num, ': ', card)
+        print('step: ', step)
+
+        while step < card_deck[num]:
+            edited_card = card.replace('  ', ' ')
+            card_win_numbers, card_numbers = edited_card.split('|')
+            card_win_numbers = card_win_numbers.strip().split(':')[1].strip().split(' ')
+            card_numbers = card_numbers.strip().split(' ')
+            card_numbers = [int(i) for i in card_numbers]
+            card_win_numbers = [int(i) for i in card_win_numbers]
+
+            hits = len([i for i in card_win_numbers if i in card_numbers])
+
+            if hits > 0:
+                points_total += 2**(hits - 1)
+
+            for j in range(1, min(hits + 1, card_nums - num + 1)):
+                card_deck[num + j] += 1
+
+            step += 1
+
+    for key in card_deck:
+        cards_total += card_deck[key]
+
+    return points_total, cards_total
 
 
 if __name__ == '__main__':
-    print(adventofcode_4_1())
+    print(adventofcode_4_2())
 
