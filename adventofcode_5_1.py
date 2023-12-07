9858e391b72eab4e9db705530e078995739de208
sanr_w <fieldstreet.27@gmail.com>
2023-12-07 12:22:57 +0300

    working on 5-2

--- adventofcode_5_1.py
+++ adventofcode_5_1.py
@@ -1,87 +1,62 @@
 
-SEED_MAP_CASH = {}
+SEED_MAP_CASH_TEMP = []
 
 
 def turn_to_dict(data: list, seed_no=None):
-
-    global SEED_MAP_CASH
-
-    SEED_MAP_CASH = {}
-
-    seed_map = {data[0]: {}}
-
-    print('proceeding', data[0])
-    print('input_seeds:', seed_no)
-
-    for seed in seed_no:
-        for num, line in enumerate(data[1:]):
-            seed_check = line.split(' ')
-            if int(seed_check[1]) <= seed <= int(seed_check[1]) + int(seed_check[2]):
-                print(f'{int(seed_check[1])} <= {seed} <= {int(seed_check[1]) + int(seed_check[2])}')
-
-                # seed_range = list(range(int(seed_check[1]), int(seed_check[1]) + int(seed_check[2]) + 1))
-                # soil_range = list(range(int(seed_check[0]), int(seed_check[0]) + int(seed_check[2]) + 1))
-                #
-                # seed_list = [int(i) for i in seed_range]
-                # soil_list = [int(j) for j in soil_range]
-
-                SEED_MAP_CASH[data[0]][seed] = int(seed_check[0]) - int(seed_check[1]) + seed
-                # seed_map[data[0]].update(dict(zip(seed_list, soil_list)))
-
-        # print(f'seed_map for line "{line}":', seed_map)
-
-    ret_dict = {}
-
-    print('FINAL seed_map: ', SEED_MAP_CASH)
-
-    for seed in seed_no:
-        if seed in SEED_MAP_CASH[data[0]]:
-            ret_dict[seed] = SEED_MAP_CASH[data[0]][seed]
-        else:
-            ret_dict[seed] = seed
-
-    soil_list = [ret_dict[i] for i in ret_dict]
-
-    print('ret_dict:', ret_dict)
-    print('soil_list:', soil_list)
-
-    return soil_list
+    pass
 
 
-def extract_seed(data):
+def start_with_extract_seeds(data):
+    """
+    turn plain list of starting seeds to 2 num tuple list
+    """
     to_int = data.strip().split(':')[1].strip().split(' ')
     as_list_of_tuples = []
     for k in range(0, len(to_int), 2):
         as_list_of_tuples.append((int(to_int[k]), int(to_int[k + 1])))
+
     return as_list_of_tuples
 
 
-def adventofcode_5_1():
-    '''
-    31599214 5-1
+def seed_soil_array_check(a: list, b: list, soil: int):
+
+    rest_of_a = []
+
+    resulting_b = []
+
+    if a[0] >= b[0]:
+        if a[0] + a[1] <= b[0] + b[1]:
+            resulting_b = [soil + (b[0] - a[0]), a[1]]
+        elif b[0] <= a[1] <= b[0] + b[1]:
+            resulting_b = [soil + (b[0] - a[0]), ]
+            rest_of_a.append([a[0] + (b[1] - (b[0] - a[0])), a[1] - (b[1] - (b[0] - a[0]))])
+    elif a[0] <= b[0]:
+        if a[0] + a[1] >= b[0] + b[1]:
+            resulting_b = [soil, b[1]]
+            rest_of_a.append([a[0], a[0] - b[0]])
+            rest_of_a.append([a[0] + (a[0] - b[0]) + b[1], a[1] - (a[0] - b[0]) - b[1]])
+        elif b[0] <= a[0] + a[1] <= b[0] + b[1]:
+            resulting_b = [soil, a[1] - (a[0] - b[0])]
+            rest_of_a.append([a[0], a[0] - b[0]])
+    return [resulting_b, rest_of_a]
 
 
-    '''
-
+def adventofcode_5_2():
+    """
+    Reads input from a file, extracts a seed number, and returns the soil number.
+    """
+    # Open the file and read the content
     with open('inputtext_test.txt') as file:
         data = file.read()
+
+    # Extract the soil number from the data
     data_start = data.strip().strip('\n').split('\n\n')
-    seed_no = extract_seed(data_start[0])
-    seed_no.sort()
+    soil_no_to_work = start_with_extract_seeds(data_start[0])
 
-    seed_no_to_work = []
-
-    for k in range(0, len(seed_no), 2):
-        seed_no_to_work += (seed_no[k], seed_no[k + 1])
-
-    for line_1 in data_start[1:]:
-        if not line_1.strip('\n') == '':
-            new_line = line_1.strip('\n').split('\n')
-            seed_no_to_work = turn_to_dict(new_line, seed_no=seed_no_to_work)
-
-    return seed_no_to_work
+    return soil_no_to_work
 
 
 if __name__ == '__main__':
-    print(adventofcode_5_1())
+    print(adventofcode_5_2())
+
 
